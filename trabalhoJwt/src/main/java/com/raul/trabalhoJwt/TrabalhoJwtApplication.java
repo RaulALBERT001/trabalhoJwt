95af01b43486c9b90e0c0fae0718a0a693596772
RAULALBERT001 <albertiraulsergio@gmail.com>
2025-06-14 11:26:19 -0300

    commit inicial

---
+++ trabalhoJwt/src/main/java/com/raul/trabalhoJwt/TrabalhoJwtApplication.java
@@ -0,0 +1,13 @@
+package com.raul.trabalhoJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoJwtApplication.class, args);
+	}
+
+}
